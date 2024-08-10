bb4caf7b7583126dc40d365ca3f4084f2132fe48
OakBoonsai <tanachotboonsai@gmail.com>
2024-08-11 01:40:17 +0700

    first done

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import  React  from 'react';
+import './style.css'
+// import Calendar from './class-component'
+import {Header, Content, Footer} from './func-component'  
+
+function App(){
+  return (
+    <>
+        <Header/>
+        <Content/>
+        <Footer/>
+    </>
+
+  )
+}
+export default App;
