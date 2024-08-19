d966a0d61d4e2d3068cf61d100416d52c61fa569
OakBoonsai <156067571+OakBoonsai@users.noreply.github.com>
2024-08-19 11:57:12 +0700

    again

--- src/refs-func.js
+++ src/refs-func.js
@@ -4,20 +4,20 @@ export default function RefsFunc(){
     const num1=useRef()
     const num2=useRef()
     const operator=React.useRef()
-    const result=useRef.useRef()
+    const result=useRef()
     const calculate=(ev)=>{
         let op=ev.target.innerText
-        let n1=parseInt(num1.current.InnerText)
-        let n2=parseInt(num2.current.InnerText)
+        let n1=parseInt(num1.current.innerText)
+        let n2=parseInt(num2.current.innerText)
         let r=eval(`${n1} ${op} ${n2}`)
-        result.current.InnerText=r
-        operator.current.InnerText=op
+        result.current.innerText=r
+        operator.current.innerText=op
     }
     return(
         <div style={{textAlign:'center',marginTop:20,fontSize:18}}>
             <span ref={num1}>15</span>&nbsp;
             <span ref={operator}>?</span>&nbsp;
-            <span ref={num1}>5</span>&nbsp;
+            <span ref={num2}>5</span>&nbsp;
             <span>=</span>&nbsp;
             <span ref={result}>?</span>
             <div>
