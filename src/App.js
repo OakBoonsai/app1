5e08c507aa58e9eebbc5499a07cf3efe4517ab49
OakBoonsai <156067571+OakBoonsai@users.noreply.github.com>
2024-08-19 12:03:43 +0700

    second file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import  React  from 'react'
 import RefsFunc from './refs-func'
+import MassageBox from './state-func'
 
 export default function App(){
   return(
-    <RefsFunc/>
+    <MassageBox/>
   )
 }
